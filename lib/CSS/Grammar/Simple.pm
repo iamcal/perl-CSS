ad8ab9daaf6949f2536864536ce9cc4123d5622b
Cal Henderson <cal@iamcal.com>
2008-07-31 20:03:44 +0000

    simple is no just a demo of the toker/lexer
    git-svn-id: https://svn.iamcal.com/public/perl/CSS/trunk@2328 ac7c8259-0de0-4f3c-bb6a-12394caf8efd

--- lib/CSS/Grammar/Simple.pm
+++ lib/CSS/Grammar/Simple.pm
@@ -10,7 +10,6 @@ sub init {
 
 	$self->{case_insensitive} = 1;
 
-if (0){
 	$self->add_toke_rule('A', "a");
 	$self->add_toke_rule('B', 'b');
 	$self->add_toke_rule('C', 'c');
@@ -25,23 +24,6 @@ if (0){
 	$self->add_lex_rule('pair', 'B C');
 
 	$self->set_base('base');
-}
-
-if (1){
-	$self->add_toke_rule('IDENT', '[a-z]+');	
-	$self->add_toke_rule('_STAR', '\\*');
-	$self->add_toke_rule('_PLUS', '\\+');
-	$self->add_toke_rule('_GREATER', '>');
-	$self->add_toke_rule('S', "[ \t\r\n\f]+");
-
-	$self->add_lex_rule('selector', 'simple_selector [ combinator simple_selector ]*');
-	$self->add_lex_rule('simple_selector', 'element_name? S*');
-	$self->add_lex_rule('element_name', 'IDENT | _STAR');
-	$self->add_lex_rule('combinator', '_PLUS S* | _GREATER S* |'); 
-
-	$self->set_base('selector');
-}
-
 }
 	
 1;
